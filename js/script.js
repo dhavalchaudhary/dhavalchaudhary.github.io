f77dfaa36b4ae2070fa555ea69b87be161ac2bc7
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2017-03-15 17:08:22 +0530

    updated website

--- js/script.js
+++ js/script.js
@@ -1,111 +1,36 @@
-$(document).ready(function () {
-    var lightOff = false;
-    $.fn.extend({
-        animateCss: function (animationName) {
-            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            $(this).addClass('animated ' + animationName).one(animationEnd, function () {
-                $(this).removeClass('animated ' + animationName);
-            });
+$(document).ready(function(){
+  $.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+  });
+  $(".typed-name").typed({
+    strings: ["<h2 class='name'>Hi, I'm Dhaval</h2>"],
+    typeSpeed: 15,
+    startDelay: 1500,
+    onStringTyped: function(){
+      $(".typed-occupation").css('display','inline-block');
+      $(".typed-name").css('display','block');
+      $(".typed-cursor").addClass('hidden');
+      $(".typed-occupation").typed({
+        strings: ["^500<h2 class='occupation'>I live off code, ^350music ^1000 and pizza</h2>"],
+        typeSpeed: 15,
+        startDelay: 0,
+        onStringTyped: function(){
+          setTimeout(function() {
+            $('.avatar').removeClass('hidden');
+            $(".typed-cursor").addClass('hidden');
+            $('.info').css('margin-top','-75px');
+          },1000);
+          setTimeout(function() {
+            $('.contact').removeClass('hidden');
+            $('.info').css('margin-top','0px');
+          },2000);
         }
-    });
-
-    function nameTimer() {
-        var names = ["Bonjour, je suis Dhaval", "Hallo, ich bin Dhaval", "Hola, soy Dhaval", "Hej, jag är Dhaval", "Hi, I'm Dhaval"];
-        var index = 0;
-
-        function displayName() {
-            $("#type-name").animateCss('fadeInUp');
-            $("#type-name").html(names[index]);
-            index = (index + 1) % names.length;
-        };
-        setInterval(displayName, 2500);
-    };
-    nameTimer();
-
-    function lightsOff(str) {
-        function swapStyleSheet(sheet) {
-            document.getElementById('pagestyle').setAttribute('href', sheet);
-        };
-        swapStyleSheet(str)
-    };
-    
-    $('#fullpage').fullpage({
-        anchors: ['home', 'about', 'work', 'contact'],
-        menu: '#menu',
-        navigation: true,
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-        autoScrolling: true,
-        afterLoad: function (anchorLink, index) {
-
-            if (index == 3 && $(window).width() > 481) {
-                $('#fp-nav').hide();
-            }
-
-        },
-        onLeave: function (index, nextIndex, direction) {
-            if (index == 3 && $(window).width() > 481) {
-                $('#fp-nav').show();
-            }
-        }
-    });
-    
-    $("#home-buttons button:nth-child(1)").click(function () {
-        $.fn.fullpage.moveTo('work', 1);
-    });
-    
-    $("#home-buttons button:nth-child(2)").click(function () {
-        $.fn.fullpage.moveTo('contact');
-    });
-    
-    $("#nav-toggle-on").click(function () {
-        $("#nav-toggle-on").addClass("hidden");
-        $("#nav-toggle-off").removeClass("hidden");
-        $("#menu").removeClass("hidden");
-        $("#menu li").animateCss("bounceInDown");
-        $("#header").css("background-color", " ");
-        $("#header").addClass("menu-style");
-        $("#fp-nav").addClass("hidden");
-        $("#footer").addClass("hidden");
-    });
-    
-    $("#nav-toggle-off").click(function () {
-        $("#nav-toggle-off").addClass("hidden");
-        $("#menu").addClass("hidden");
-        $("#nav-toggle-on").removeClass("hidden");
-        $("#header").removeClass("menu-style");
-        $("#fp-nav").removeClass("hidden");
-        $("#footer").removeClass("hidden");
-    });
-    
-    $("#menu li").click(function () {
-        $("#menu").addClass("hidden");
-        $(this).addClass("active");
-        $("#nav-toggle-off").addClass("hidden");
-        $("#nav-toggle-on").removeClass("hidden");
-        $("#header").removeClass("menu-style");
-        $("#fp-nav").removeClass("hidden");
-        $("#footer").removeClass("hidden");
-    });
-    
-    $("#touch-navigation-next").click(function () {
-        $.fn.fullpage.moveSectionDown();
-    });
-    
-    $("#touch-navigation-previous").click(function () {
-        $.fn.fullpage.moveSectionUp();
-    });
-
-    $("#night-mode").click(function () {
-        if (lightOff === false) {
-            $("#night-mode b").html("Lights On");
-            lightsOff('css/style-dark.css');
-            lightOff = true;
-        } else {
-            $("#night-mode b").html("Lights Off");
-            lightsOff('css/style-light.css');
-            lightOff = false;
-        }
-
-    });
+      });
+    }
+  });
 });
